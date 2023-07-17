cdbc85b4156d6e1c847d2116afaeeb2bda0abfdb
black <no_email@email.com>
2023-07-17 23:12:45 +0400

    response_model=State_status

--- app_salt_keys/routes/kyes.py
+++ app_salt_keys/routes/kyes.py
@@ -5,51 +5,73 @@ from app_salt_keys.auth import get_api_key
 from app_salt_keys.salt_keys import chek_keys_in_accepted
 from app_salt_keys.salt_keys import key_accept
 from app_salt_keys.salt_keys import key_delete
+from pydantic import BaseModel
+
+
+class State_status(BaseModel):
+    state: str = 'ok'
+
 
 
 router = APIRouter(
     prefix="/api/v1",
     tags=["keys"],
-    dependencies=[Depends(get_api_key)],
+    dependencies=[Depends(get_api_key)]
 )
 
 
-@router.get("/keys/{key}", status_code=status.HTTP_200_OK)
-def key_check(key: str, response: Response):
+@router.get("/keys/{key}", status_code=status.HTTP_200_OK, response_model=State_status)
+def key_check(key: str, response: Response) -> dict:
     try:
         if chek_keys_in_accepted(key) is True:
-            return {'state': 'ok'}
+            response.headers["Content-Type"] = "application/json"
+            state = State_status(state='ok')
+            return state
         else:
             response.status_code = status.HTTP_404_NOT_FOUND
-            return {'ERROR': 'kye was not found'}
+            response.headers["Content-Type"] = "application/json"
+            state = State_status(state='kye was not found')
+            return state
     except (CalledProcessError, FileNotFoundError, PermissionError):
         response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-        return {'ERROR': 'Cannot run salt-kyes'}
+        response.headers["Content-Type"] = "application/json"
+        state = State_status(state='Cannot run salt-kyes')
+        return state
 
 
-@router.post("/keys/{key}", status_code=status.HTTP_201_CREATED)
-def add_key(key: str, response: Response):
+@router.post("/keys/{key}", status_code=status.HTTP_201_CREATED, response_model=State_status)
+def add_key(key: str, response: Response) -> dict:
     try:
         if chek_keys_in_accepted(key) is True:
-            r 
-            return {'state': 'ok'}
-        elif key_accept(key) is True:
-            return {'state': 'ok'}
+            key_accept(key)
+            response.headers["Content-Type"] = "application/json"
+            state = State_status(state='ok')
+            return state
         else:
             response.status_code = status.HTTP_404_NOT_FOUND
-            return {'ERROR': f'The key glob {key} does not match any unaccepted keys.'}
+            response.headers["Content-Type"] = "application/json"
+            state = State_status(state=f'The key glob {key} does not match any unaccepted keys.')
+            return state
     except(CalledProcessError, FileNotFoundError, PermissionError):
         response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-        return {'ERROR': 'Cannot run salt-kyes'}
+        response.headers["Content-Type"] = "application/json"
+        state = State_status(state='Cannot run salt-kyes')
+        return state
     except:
-        return {'ERROR': 'Unknown error'}
+        state = State_status(state='Unknown error')
+        response.headers["Content-Type"] = "application/json"
+        return state
 
 
-@router.delete("/keys/{key}", status_code=status.HTTP_200_OK)
-def keys_delete(key: str, response: Response):
+@router.delete("/keys/{key}", status_code=status.HTTP_200_OK, response_model=State_status)
+def keys_delete(key: str, response: Response) -> dict:
     try:
         key_delete(key)
-        return {'state': 'ok'}
+        response.headers["Content-Type"] = "application/json"
+        state = State_status(state='ok')
+        return state
     except (CalledProcessError, FileNotFoundError, PermissionError):
         response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-        return {'ERROR': 'Cannot run salt-kyes'}
+        state = State_status(state='Cannot run salt-kyes')
+        response.headers["Content-Type"] = "application/json"
+        return state
